8ab146fc02fcc727e015e11d793dd056ae656a5a
mariaroman99 <mromanma86@alumnes.ub.edu>
2021-02-26 12:10:43 +0100

    Entrega opcional pis

---
+++ app/src/main/java/com/example/calculadorabodymassindex/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.example.calculadorabodymassindex
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.content.Intent
+import com.google.android.material.snackbar.Snackbar
+import kotlinx.android.synthetic.main.activity_main.*
+import java.math.BigDecimal
+import java.math.RoundingMode
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        calcular_btn.setOnClickListener{
+            if (altura.text.isNotEmpty() && peso.text.isNotEmpty()) {
+                val a:Float = altura.text.toString().toFloat() /100
+                val p:Float = peso.text.toString().toFloat()
+                val resultado:Float = p/(a*a)
+                resultado.text = "%.2f".format(resultado)
+        }   else {
+                showErrorSnack("Incorrect Values")
+            }
+        }   else {
+                showErrorSnack("A filed is missing")
+    }
+        }
+    }
+}
